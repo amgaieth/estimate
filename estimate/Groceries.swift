c9b60a37dbbc8ee2e6976a314cbb002c0b034e6f
amgaieth <amgaieth@me.com>
2016-08-04 18:35:21 -0700

    added class Sports & class Transportation

--- estimate/Groceries.swift
+++ estimate/Groceries.swift
@@ -29,4 +29,24 @@ class Groceries: UITableViewController {
     @IBOutlet weak var product18Label: UILabel!
     @IBOutlet weak var product19Label: UILabel!
 
+    @IBOutlet weak var price1Label: UILabel!
+    @IBOutlet weak var price2Label: UILabel!
+    @IBOutlet weak var price3Label: UILabel!
+    @IBOutlet weak var price4Label: UILabel!
+    @IBOutlet weak var price5Label: UILabel!
+    @IBOutlet weak var price6Label: UILabel!
+    @IBOutlet weak var price7Label: UILabel!
+    @IBOutlet weak var price8Label: UILabel!
+    @IBOutlet weak var price9Label: UILabel!
+    @IBOutlet weak var price10Label: UILabel!
+    @IBOutlet weak var price11Label: UILabel!
+    @IBOutlet weak var price12Label: UILabel!
+    @IBOutlet weak var price13Label: UILabel!
+    @IBOutlet weak var price14Label: UILabel!
+    @IBOutlet weak var price15Label: UILabel!
+    @IBOutlet weak var price16Label: UILabel!
+    @IBOutlet weak var price17Label: UILabel!
+    @IBOutlet weak var price18Label: UILabel!
+    @IBOutlet weak var price19Label: UILabel!
+    
 }
---
+++ estimate/SportsAndLeisure.swift
@@ -0,0 +1,20 @@
+//
+//  SportsAndLeisure.swift
+//  estimate
+//
+//  Created by Anouar Mgaieth on 8/4/16.
+//  Copyright © 2016 Anouar Mgaieth. All rights reserved.
+//
+
+import UIKit
+
+class SportsAndLeisure: UITableViewController {
+    
+    @IBOutlet weak var product1Label: UILabel!
+    @IBOutlet weak var product2Label: UILabel!
+    @IBOutlet weak var product3Label: UILabel!
+
+    @IBOutlet weak var price1Label: UILabel!
+    @IBOutlet weak var price2Label: UILabel!
+    @IBOutlet weak var price3Label: UILabel!
+}
